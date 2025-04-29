7cbbcc189c4e22f32086e4a9fafc415616b81312
Emma Zheng <emmaz@mit.edu>
2025-04-29 10:15:09 -0400

    emma iter 0: integration

---
+++ iter0/emmaz/integration-tests.ts
@@ -0,0 +1,31 @@
+// move these to their respective test files when created
+
+// most parts can be tested automatically, but add the following:
+
+// client.test.ts
+
+// Manual test: First puzzle
+// Covers: unsolved puzzle state, getting blank puzzle, TODO: fix based on testing strategy partitions
+// 1. go to server website and assert that the blank puzzle board is displayed
+
+// Manual test: Adding star but still unsolved
+// Covers: unsolved puzzle state, adding star, TODO: fix based on testing strategy partitions
+// 1. go to server website and assert that the puzzle board is displayed
+// 2. click on an empty square and assert that star appears
+
+// Manual test: Removing star but still unsolved
+// Covers: unsolved puzzle state, removing star, TODO: fix based on testing strategy partitions
+// 1. go to server website and assert that the puzzle board is displayed
+// 2. click on a square with star and assert that star disappears
+
+// Manual test: Adding star and solved
+// Covers: solved puzzle state, adding star, TODO: fix based on testing strategy partitions
+// 1. go to server website and assert that the puzzle board is displayed
+// 2. click on an empty square and assert that star appears
+// 3. assert that a puzzle solved message is displayed
+
+// Manual test: Removing star and solved
+// Covers: solved puzzle state, removing star, TODO: fix based on testing strategy partitions
+// 1. go to server website and assert that the puzzle board is displayed
+// 2. click on a square with star and assert that star disappears
+// 3. assert that a puzzle solved message is displayed
